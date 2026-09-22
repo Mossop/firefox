b736e0268f0d538b3ae07143694b75a560588e24
longsonr <longsonr@gmail.com>
2026-09-22 18:11:07 -0400

    Bug 2074212 a=dsmith
    Differential Revision: https://phabricator.services.mozilla.com/D328295

--- layout/svg/SVGObserverUtils.cpp
+++ layout/svg/SVGObserverUtils.cpp
@@ -1207,6 +1207,7 @@ NS_DECLARE_FRAME_PROPERTY_RELEASABLE(BackgroundClipObserverProperty,
 template <class T>
 static T* GetEffectProperty(URLAndReferrerInfo* aURI, nsIFrame* aFrame,
                             const FramePropertyDescriptor<T>* aProperty) {
+  MOZ_ASSERT(!aFrame->GetPrevContinuation(), "Require first continuation");
   if (!aURI) {
     return nullptr;
   }
@@ -1363,6 +1364,9 @@ static SVGPaintingProperty* GetOrCreateClipPathObserver(
 
 SVGObserverUtils::ReferenceState SVGObserverUtils::GetAndObserveClipPath(
     nsIFrame* aClippedFrame, SVGClipPathFrame** aClipPathFrame) {
+  // Continuations can come and go during reflow, and we don't need to observe
+  // the referenced element more than once for a given node.
+  aFrame = aFrame->FirstContinuation();
   if (aClipPathFrame) {
     *aClipPathFrame = nullptr;
   }
@@ -1538,6 +1542,9 @@ void SVGObserverUtils::RemoveTemplateObserver(nsIFrame* aFrame) {
 
 Element* SVGObserverUtils::GetAndObserveBackgroundImage(nsIFrame* aFrame,
                                                         const nsAtom* aHref) {
+  // Continuations can come and go during reflow, and we don't need to observe
+  // the referenced element more than once for a given node.
+  aFrame = aFrame->FirstContinuation();
   bool found;
   URIObserverHashtable* hashtable =
       aFrame->GetProperty(BackgroundImageProperty(), &found);
