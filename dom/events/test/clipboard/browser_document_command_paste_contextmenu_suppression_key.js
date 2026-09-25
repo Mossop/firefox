998fe2058fc7de58044d2c7b07c17595e8578dd2
Florian Quèze <florian@queze.net>
2026-09-25 23:13:51 +0000

    Bug 2075369 - Wait for the paste popup dismissal clicks before ending each subtest of browser_document_command_paste_contextmenu_suppression_key.js, r=dom-core-reviewers,edgar.
    Differential Revision: https://phabricator.services.mozilla.com/D329182

--- dom/events/test/clipboard/browser_document_command_paste_contextmenu_suppression_key.js
+++ dom/events/test/clipboard/browser_document_command_paste_contextmenu_suppression_key.js
@@ -82,16 +82,20 @@ async function runKeyEventTest(
   aKeyboardEventType,
   aShouldSuppressContextMenu
 ) {
+  let dismissPromise;
   let listener = function (e) {
     if (e.target.getAttribute("id") == kPasteMenuPopupId) {
       ok(
         !aShouldSuppressContextMenu,
         `paste contextmenu should ${aShouldSuppressContextMenu ? "not " : ""}be shown`
       );
-      SpecialPowers.executeSoon(async () => {
-        await promiseDismissPasteButton();
-        // XXX not sure why first promiseDismissPasteButton doesn't work on Windows opt build.
-        await promiseDismissPasteButton();
+      dismissPromise = new Promise(resolve => {
+        SpecialPowers.executeSoon(async () => {
+          await promiseDismissPasteButton();
+          // XXX not sure why first promiseDismissPasteButton doesn't work on Windows opt build.
+          await promiseDismissPasteButton();
+          resolve();
+        });
       });
     }
   };
@@ -109,6 +113,7 @@ async function runKeyEventTest(
     await EventUtils.synthesizeAndWaitKey(aKey, aModifiers);
     let result = await resultPromise;
     is(result, aShouldSuppressContextMenu, `Check execCommand("paste") result`);
+    await dismissPromise;
   });
 
   document.removeEventListener("popupshown", listener);
