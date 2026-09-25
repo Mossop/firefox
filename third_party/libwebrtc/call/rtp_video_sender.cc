4f09ceffef983ce0461c1d6665bd48a43a3a3e94
Jesús Leganés-Combarro 'piranna <piranna@gmail.com>
2026-09-25 19:30:51 +0000

    Bug 2063114 - Cherry-pick libwebrtc v119 6fc4d9750c::52bc9f7c12 r=mjf a=dmeehan
    Detailed-Range-First: 9c0da0b9a0914e0bbd365976cac9fe91402ba1f3
    Detailed-Range-Last: 454ab49f9fd5aebe8c9843b2b333e45de04870f4
    Upstream-Range-First: 6fc4d9750c
    Upstream-Range-Last: 52bc9f7c12
    Detailed-Commit-Count: 164
    Detailed-Stack-Head: 454ab49f9fd5aebe8c9843b2b333e45de04870f4
    Detailed-Endpoint-Tree: e58d352e4fb1c61dfa1b4e50c5f8eb6def8f78c8
    Source-Orders-First: 81
    Source-Orders-Last: 163
    
    Differential Revision: https://phabricator.services.mozilla.com/D323720

--- third_party/libwebrtc/call/rtp_video_sender.cc
+++ third_party/libwebrtc/call/rtp_video_sender.cc
@@ -508,6 +508,7 @@ void RtpVideoSender::SetActiveModulesLocked(
     const bool was_active = rtp_module.Sending();
     const bool should_be_active = active_modules[i];
 
+    // Sends a kRtcpByeCode when going from true to false.
     rtp_module.SetSendingStatus(active_modules[i]);
 
     if (was_active && !should_be_active) {
@@ -575,7 +576,7 @@ EncodedImageCallback::Result RtpVideoSender::OnEncodedImage(
   RTC_DCHECK_LT(simulcast_index, rtp_streams_.size());
 
   uint32_t rtp_timestamp =
-      encoded_image.Timestamp() +
+      encoded_image.RtpTimestamp() +
       rtp_streams_[simulcast_index].rtp_rtcp->StartTimestamp();
 
   // RTCPSender has it's own copy of the timestamp offset, added in
@@ -583,7 +584,7 @@ EncodedImageCallback::Result RtpVideoSender::OnEncodedImage(
   // TODO(nisse): Delete RTCPSender:timestamp_offset_, and see if we can confine
   // knowledge of the offset to a single place.
   if (!rtp_streams_[simulcast_index].rtp_rtcp->OnSendingRtpFrame(
-          encoded_image.Timestamp(), encoded_image.capture_time_ms_,
+          encoded_image.RtpTimestamp(), encoded_image.capture_time_ms_,
           rtp_config_.payload_type,
           encoded_image._frameType == VideoFrameType::kVideoFrameKey)) {
     // The payload router could be active but this module isn't sending.
